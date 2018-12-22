86b5a41d75a2200f2f7c7075c0a40780a982bda9
Morten Ebbesen <mentry@mtedev.onmicrosoft.com>
2018-12-22 19:32:21 +0100

    Add project files.

---
+++ ETP/Controllers/HomeController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ETP.Models;
+
+namespace ETP.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        /*  Travel pages  */
+
+        public IActionResult TravelCategory()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Travel()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult TravelPlan()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        /*  Booking pages  */
+
+        public IActionResult Booking()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        /* User pages*/
+
+        public IActionResult MyTravels()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult MyInformation()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
