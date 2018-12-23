16196805d1d2c566b97118b6089195f8439d20bd
Morten Ebbesen <mentry@mtedev.onmicrosoft.com>
2018-12-24 00:31:58 +0100

    Update XXYY

--- ETP.AzureFunctions/Function1.cs
+++ ETP.AzureFunctions/Function1.cs
@@ -23,7 +23,7 @@ namespace ETP.AzureFunctions
             name = name ?? data?.name;
 
             return name != null
-                ? (ActionResult)new OkObjectResult($"Hello you 3333:, {name}")
+                ? (ActionResult)new OkObjectResult($"Hello you 4444555:, {name}")
                 : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
         }
     }
