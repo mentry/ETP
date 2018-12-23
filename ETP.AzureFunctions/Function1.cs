2fceff3f4c1752cd125719334ff1becec71d67d2
Morten Ebbesen <mentry@mtedev.onmicrosoft.com>
2018-12-23 19:50:15 +0100

    Update v5

--- ETP.AzureFunctions/Function1.cs
+++ ETP.AzureFunctions/Function1.cs
@@ -9,9 +9,9 @@ using Newtonsoft.Json;
 
 namespace ETP.AzureFunctions
 {
-    public static class Function1
+    public static class FunctionApp
     {
-        [FunctionName("Function1")]
+        [FunctionName("FunctionApp")]
         public static IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]HttpRequest req, TraceWriter log)
         {
             log.Info("C# HTTP trigger function processed a request.");
@@ -24,6 +24,24 @@ namespace ETP.AzureFunctions
 
             return name != null
                 ? (ActionResult)new OkObjectResult($"Helloee333e:, {name}")
+                : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
+        }
+    }
+    public static class FunctionApp2
+    {
+        [FunctionName("FunctionApp2")]
+        public static IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]HttpRequest req, TraceWriter log)
+        {
+            log.Info("C# HTTP trigger function processed a request.");
+
+            string name = req.Query["name"];
+
+            string requestBody = new StreamReader(req.Body).ReadToEnd();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            name = name ?? data?.name;
+
+            return name != null
+                ? (ActionResult)new OkObjectResult($"Helloee2233:, {name}")
                 : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
         }
     }
